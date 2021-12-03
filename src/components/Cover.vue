c5fe6a928e6188822c45eeb60447df221c5b883b
kora <kangrun@hhx-info.com>
2021-12-03 15:30:55 +0800

    resume - 部分优化

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -1,7 +1,7 @@
 <template>
-  <section :class="[ { cover: 'cover' }, resumeShow ]" id="impress-toolbar">
+  <section :class="[{ cover: 'cover' }, resumeShow]" id="impress-toolbar">
     <main>
-      <img src="../assets/KR.jpg" alt="">
+      <img src="../assets/KR.jpg" alt="" />
 
       <h1>
         <svg width="100%" height="60">
@@ -28,148 +28,201 @@
 
     <!--双圆绘制 -->
     <!--<div class="double_round">-->
-      <!--<img src="../assets/img/zfb.jpg" alt="">-->
+    <!--<img src="../assets/img/zfb.jpg" alt="">-->
     <!--</div>-->
   </section>
 </template>
 
 <script>
-  export default {
-    name: 'Cover',
-    data() {
-      return {
-        // resumeShow: ''
-      }
+export default {
+  name: "Cover",
+  data() {
+    return {
+      // resumeShow: ''
+    };
+  },
+  props: {
+    resumeShow: String,
+  },
+  computed: {},
+  mounted() {
+    this.$route.params.flag && this.$router.go(0);
+  },
+  methods: {
+    // 跳转 - PHP页面展示
+    handleClickPhpShow() {
+      this.$router.push({ name: "php" });
+      // this.$router.go(0)
     },
-    props: {
-      resumeShow: String
+    // 下滑 - css页面展示
+    handleClickCssShow() {
+      this.$emit("showCss");
     },
-    computed: {},
-    mounted(){
-      this.$route.params.flag && this.$router.go(0)
-    },
-    methods: {
-      // 跳转 - PHP页面展示
-      handleClickPhpShow() {
-        this.$router.push({name:'php'})
-        this.$router.go(0)
-      },
-      // 下滑 - css页面展示
-      handleClickCssShow() {
-        this.$emit('showCss')
-      }
-    }
-  }
-
+  },
+};
 </script>
 
 <style scoped>
-  .cover{position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); z-index: 9999; width: 100%; height: 100%; background: #eee; text-align: center; transition: all 1.3s ease-in-out}
-  .cover>main{position: absolute; top: 40%; left: 50%; transform: translate(-50%, -50%)}
-  .cover>main>h1{margin-top: 20px; font-size: 24px; color: #dedede}
-  .btn_all{display: flex; justify-content: center; align-items: center; padding: 20px; box-sizing: border-box; color: #72b182; font-size: 20px}
-  .btn_all>div{position: relative; top: 0; left: 0; margin: 30px; cursor: pointer}
-  .btn_all>div::after{content: ''; position: absolute; bottom: -5px; left: 50%; transform: translateX(-50%); width: 10%; height: 100%; border-bottom: 2px solid #72b182; transition: all .6s}
-  .btn_all>div:hover::after{width: 100%}
-  .cover>main>img{width: 200px; animation: jump 1s infinite linear}
-  @keyframes jump {
-    0% {
-      transform: translate(0px, 0px);
-    }
-    50% {
-      transform: translate(0px, -10px);
-    }
-    100% {
-      transform: translate(0px, 0px);
-    }
+.cover {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+  width: 100%;
+  height: 100%;
+  background: #eee;
+  text-align: center;
+  transition: all 1.3s ease-in-out;
+}
+.cover > main {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.cover > main > h1 {
+  margin-top: 20px;
+  font-size: 24px;
+  color: #dedede;
+}
+.btn_all {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  color: #72b182;
+  font-size: 20px;
+}
+.btn_all > div {
+  position: relative;
+  top: 0;
+  left: 0;
+  margin: 30px;
+  cursor: pointer;
+}
+.btn_all > div::after {
+  content: "";
+  position: absolute;
+  bottom: -5px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 10%;
+  height: 100%;
+  border-bottom: 2px solid #72b182;
+  transition: all 0.6s;
+}
+.btn_all > div:hover::after {
+  width: 100%;
+}
+.cover > main > img {
+  width: 200px;
+  animation: jump 1s infinite linear;
+}
+@keyframes jump {
+  0% {
+    transform: translate(0px, 0px);
   }
-  .cover>main>img{border-radius: 50%}
+  50% {
+    transform: translate(0px, -10px);
+  }
+  100% {
+    transform: translate(0px, 0px);
+  }
+}
+.cover > main > img {
+  border-radius: 50%;
+}
 
-  .resumeShow{top: 150%}
-  .resumeHide{top: 50%}
+.resumeShow {
+  top: 150%;
+}
+.resumeHide {
+  top: 50%;
+}
 
-  /*动画 - 选择不同走向展示个人信息*/
-  /*.coverAddClass{animation: cover 1.4s linear forwards}*/
-  /*@keyframes cover {*/
-    /*0% {*/
-      /*transform: translate(-30%, -30%);*/
-    /*}*/
-    /*20%{*/
-      /*transform: translate(-10%, -10%) scale(.8);*/
-      /*border-radius: 10%;*/
-    /*}*/
-    /*40%{*/
-      /*transform: translate(10%, 10%) scale(.7);*/
-      /*border-radius: 20%;*/
-    /*}*/
-    /*60%{*/
-      /*transform: translate(30%, 30%) scale(.6);*/
-      /*border-radius: 30%;*/
-    /*}*/
-    /*80%{*/
-      /*transform: translate(50%, 50%) scale(.5);*/
-      /*border-radius: 40%;*/
-    /*}*/
-    /*90%{*/
-      /*transform: translate(60%, 50%) scale(.5);*/
-      /*border-radius: 50%;*/
-    /*}*/
-    /*95%{*/
-      /*transform: translate(65%, 50%) scale(.5);*/
-      /*border-radius: 50%;*/
-    /*}*/
-    /*100% {*/
-      /*transform: translate(70%, 50%) scale(.5);*/
-      /*border-radius: 50%*/
-    /*}*/
-  /*}*/
+/*动画 - 选择不同走向展示个人信息*/
+/*.coverAddClass{animation: cover 1.4s linear forwards}*/
+/*@keyframes cover {*/
+/*0% {*/
+/*transform: translate(-30%, -30%);*/
+/*}*/
+/*20%{*/
+/*transform: translate(-10%, -10%) scale(.8);*/
+/*border-radius: 10%;*/
+/*}*/
+/*40%{*/
+/*transform: translate(10%, 10%) scale(.7);*/
+/*border-radius: 20%;*/
+/*}*/
+/*60%{*/
+/*transform: translate(30%, 30%) scale(.6);*/
+/*border-radius: 30%;*/
+/*}*/
+/*80%{*/
+/*transform: translate(50%, 50%) scale(.5);*/
+/*border-radius: 40%;*/
+/*}*/
+/*90%{*/
+/*transform: translate(60%, 50%) scale(.5);*/
+/*border-radius: 50%;*/
+/*}*/
+/*95%{*/
+/*transform: translate(65%, 50%) scale(.5);*/
+/*border-radius: 50%;*/
+/*}*/
+/*100% {*/
+/*transform: translate(70%, 50%) scale(.5);*/
+/*border-radius: 50%*/
+/*}*/
+/*}*/
 
-  /*动态文字*/
-  .text{
-    font-size: 18px;
-    font-weight: 300;
-    fill: none;
-    stroke-width: 1px;
-    stroke-dasharray: 310;
-    animation: stroke 6s infinite linear;
-  }
-  .text-1{
-    stroke: #99cca1;
-    animation-delay: -1.5s;
-  }
-  .text-2 {
-    stroke: #99cca1;
-    animation-delay: -3s;
-  }
-  .text-3{
-    stroke: #99cca1;
-    animation-delay: -4.5s;
-  }
-  .text-4 {
-    stroke: #99cca1;
-    animation-delay: -6s;
-  }
+/*动态文字*/
+.text {
+  font-size: 18px;
+  font-weight: 300;
+  fill: none;
+  stroke-width: 1px;
+  stroke-dasharray: 310;
+  animation: stroke 6s infinite linear;
+}
+.text-1 {
+  stroke: #99cca1;
+  animation-delay: -1.5s;
+}
+.text-2 {
+  stroke: #99cca1;
+  animation-delay: -3s;
+}
+.text-3 {
+  stroke: #99cca1;
+  animation-delay: -4.5s;
+}
+.text-4 {
+  stroke: #99cca1;
+  animation-delay: -6s;
+}
 
-  @keyframes stroke {
-    100% {
-      stroke-dashoffset: -400;
-    }
+@keyframes stroke {
+  100% {
+    stroke-dashoffset: -400;
   }
+}
 
-
-  /*.double_round{*/
-    /*position: fixed;*/
-    /*top: 0; left: 0;*/
-    /*z-index: 9999;*/
-    /*width: 100px;*/
-    /*height: 100px;*/
-    /*padding: 3px;*/
-    /*color: green;*/
-    /*border: 4px solid currentColor;*/
-    /*border-radius: 50%;*/
-    /*box-sizing: border-box;*/
-    /*background-clip: content-box;*/
-    /*background-color: #eee;*/
-  /*}*/
-  /*.double_round>img{position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 86px; height: 86px; border-radius: 50%}*/
+/*.double_round{*/
+/*position: fixed;*/
+/*top: 0; left: 0;*/
+/*z-index: 9999;*/
+/*width: 100px;*/
+/*height: 100px;*/
+/*padding: 3px;*/
+/*color: green;*/
+/*border: 4px solid currentColor;*/
+/*border-radius: 50%;*/
+/*box-sizing: border-box;*/
+/*background-clip: content-box;*/
+/*background-color: #eee;*/
+/*}*/
+/*.double_round>img{position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 86px; height: 86px; border-radius: 50%}*/
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,46 +1,53 @@
 <template>
   <section id="impress-toolbar" class="css_all">
     <!--首页导航-->
-    <cover :coverAdd="coverShow"  @showCss="showCss" :resumeShow="resumeShow"></cover>
+    <cover
+      :coverAdd="coverShow"
+      @showCss="showCss"
+      :resumeShow="resumeShow"
+    ></cover>
 
     <!--代码 - 左侧-->
     <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
     <!--显示 - 右侧-->
-    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+    <ResumeEditor
+      ref="resumeEditor"
+      :markdown="currentMarkdown"
+      :enableHtml="enableHtml"
+    ></ResumeEditor>
 
     <aside class="back" @click="handleClickShowOrder">
-      <span>导航 - 序幕</span>
+      <span>返回首页</span>
       <svg v-if="svgShow" height="50" width="150">
-        <rect id="shape" height="50" width="150"/>
+        <rect id="shape" height="50" width="150" />
       </svg>
     </aside>
-
   </section>
 </template>
 
 <script>
-  import cover from '../components/Cover'
-  import StyleEditor from '../components/StyleEditor'
-  import ResumeEditor from '../components/ResumeEditor'
-  import '../assets/reset.css'
+import cover from "../components/Cover";
+import StyleEditor from "../components/StyleEditor";
+import ResumeEditor from "../components/ResumeEditor";
+import "../assets/reset.css";
 
-  export default {
-    name: 'home',
-    components: {
-      cover,
-      StyleEditor,
-      ResumeEditor
-    },
-    data() {
-      return {
-        svgShow: false,
-        resumeShow: '',
-        coverShow: true,
-        interval: 1,
-        currentStyle: '',
-        enableHtml: false,
-        fullStyle: [
-          `/* KORA - 个人简历 */
+export default {
+  name: "home",
+  components: {
+    cover,
+    StyleEditor,
+    ResumeEditor,
+  },
+  data() {
+    return {
+      svgShow: false,
+      resumeShow: "",
+      coverShow: true,
+      interval: 1,
+      currentStyle: "",
+      enableHtml: false,
+      fullStyle: [
+        `/* KORA - 个人简历 */
 
 /* 首先，*元素添加过渡效果 */
 * {
@@ -92,14 +99,13 @@ html {
 /* 开始编辑个人信息咯~ */
 
 `,
-          `
+        `
 /* 纯文本 - 添点样式
  * Markdown 格式怎么样，对咱们的HR是不是更友好点 ^_^
  * 开源的marked时候能满足这个需求
  */
-`
-          ,
-          `
+`,
+        `
 /* 再对 HTML 加点样式 */
 .resumeEditor{
   padding: 2em;
@@ -131,10 +137,10 @@ html {
   padding: .5em;
   background: #ddd;
 }
-`],
-        currentMarkdown: '',
-        fullMarkdown:
-          `康润
+`,
+      ],
+      currentMarkdown: "",
+      fullMarkdown: `康润
 ----
 
 前端工程师
@@ -166,103 +172,156 @@ html {
 
 :::tip 点击底部 - 导航 - 查看PPT，更多资料等你解锁~  ☀☀☀☀☀
 
-`
-      }
+`,
+    };
+  },
+  async created() {
+    // 默认开始不展示,通过点击事件触发
+    // this.makeResume()
+  },
+  beforeCreate() {},
+  methods: {
+    showCss() {
+      this.resumeShow = "resumeShow";
+      this.makeResume();
     },
-    async created() {
-      // 默认开始不展示,通过点击事件触发
-      // this.makeResume()
+    // 动态展示 - css
+    async makeResume() {
+      await this.progressivelyShowStyle(0).then(() => {
+        console.log(0);
+      });
+      await this.progressivelyShowResume().then(() => {
+        console.warn("简直不要太棒了1");
+      });
+      await this.progressivelyShowStyle(1).then(() => {
+        console.log(1);
+      });
+      await this.showHtml().then(() => {
+        console.warn("简直不要太棒了2");
+      });
+      await this.progressivelyShowStyle(2).then(() => {
+        console.log(2), (this.svgShow = true);
+      });
     },
-    beforeCreate(){},
-    methods: {
-      showCss() {
-        this.resumeShow = 'resumeShow'
-        this.makeResume()
-      },
-      // 动态展示 - css
-      async makeResume() {
-        await this.progressivelyShowStyle(0)
-          .then(()=> {console.log(0)})
-        await this.progressivelyShowResume()
-          .then((()=> {console.warn('简直不要太棒了1')}))
-        await this.progressivelyShowStyle(1)
-          .then(()=> {console.log(1)})
-        await this.showHtml()
-          .then((()=> {console.warn('简直不要太棒了2')}))
-        await this.progressivelyShowStyle(2)
-          .then(()=> {console.log(2), this.svgShow = true})
-      },
-      showHtml() {
-        return new Promise((resolve, reject) => {
-          this.enableHtml = true
-          resolve()
-        })
-      },
-      progressivelyShowStyle(n) {
-        return new Promise((resolve, reject) => {
-          let interval = this.interval
-          let showStyle = (async function () {
-            let style = this.fullStyle[n]
-            if (!style) { return }
-            // 计算前 n 个 style 的字符总数
-            // console.log(this.fullStyle.filter((_, index, _____________) => index <= n), 777)
-            let length = this.fullStyle.filter((_, index, _____________) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
-            let prefixLength = length - style.length
-            if (this.currentStyle.length < length) {
-              let l = this.currentStyle.length - prefixLength
-              let char = style.substring(l, l + 1) || ' '
-              this.currentStyle += char
-              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
-                this.$nextTick(() => {
-                  this.$refs.styleEditor.goBottom()
-                })
-              }
-              setTimeout(showStyle, interval)
-            } else {
-              resolve()
-            }
-          }).bind(this)
-          showStyle()
-        })
-      },
-      progressivelyShowResume() {
-        return new Promise((resolve, reject) => {
-          let length = this.fullMarkdown.length
-          let interval = this.interval
-          let showResume = () => {
-            if (this.currentMarkdown.length < length) {
-              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
-              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
-              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
-              if (prevChar === '\n' && this.$refs.resumeEditor) {
-                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
-              }
-              setTimeout(showResume, interval)
-            } else {
-              resolve()
-            }
+    showHtml() {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true;
+        resolve();
+      });
+    },
+    progressivelyShowStyle(n) {
+      return new Promise((resolve, reject) => {
+        let interval = this.interval;
+        let showStyle = async function () {
+          let style = this.fullStyle[n];
+          if (!style) {
+            return;
           }
-          showResume()
-        })
-      },
+          // 计算前 n 个 style 的字符总数
+          // console.log(this.fullStyle.filter((_, index, _____________) => index <= n), 777)
+          let length = this.fullStyle
+            .filter((_, index, _____________) => index <= n)
+            .map((item) => item.length)
+            .reduce((p, c) => p + c, 0);
+          let prefixLength = length - style.length;
+          if (this.currentStyle.length < length) {
+            let l = this.currentStyle.length - prefixLength;
+            let char = style.substring(l, l + 1) || " ";
+            this.currentStyle += char;
+            if (style.substring(l - 1, l) === "\n" && this.$refs.styleEditor) {
+              this.$nextTick(() => {
+                this.$refs.styleEditor.goBottom();
+              });
+            }
+            setTimeout(showStyle, interval);
+          } else {
+            resolve();
+          }
+        }.bind(this);
+        showStyle();
+      });
+    },
+    progressivelyShowResume() {
+      return new Promise((resolve, reject) => {
+        let length = this.fullMarkdown.length;
+        let interval = this.interval;
+        let showResume = () => {
+          if (this.currentMarkdown.length < length) {
+            this.currentMarkdown = this.fullMarkdown.substring(
+              0,
+              this.currentMarkdown.length + 1
+            );
+            let lastChar =
+              this.currentMarkdown[this.currentMarkdown.length - 1];
+            let prevChar =
+              this.currentMarkdown[this.currentMarkdown.length - 2];
+            if (prevChar === "\n" && this.$refs.resumeEditor) {
+              this.$nextTick(() => this.$refs.resumeEditor.goBottom());
+            }
+            setTimeout(showResume, interval);
+          } else {
+            resolve();
+          }
+        };
+        showResume();
+      });
+    },
 
-      handleClickShowOrder() {
-        this.resumeShow = 'resumeHide'
-      }
-    }
-  }
-
+    handleClickShowOrder() {
+      this.resumeShow = "resumeHide";
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-  html {min-height: 100vh;}
-  *{box-sizing: border-box;}
-  .css_all{height: 92vh}
-  #impress-toolbar{opacity: 1!important;}
-  #shape { stroke-width: 6px; fill: transparent; stroke: #222; stroke-dasharray: 104 300; stroke-dashoffset: -220; transition: 1s all ease}
-  svg:hover #shape { stroke-dasharray: 70 0; stroke-width: 3px; stroke-dashoffset: 0; stroke: #72b182}
-  .back{position: fixed; bottom: 2px; left: 50%; transform: translateX(-50%); width: 150px; height: 52px; font-size: 14px; cursor: pointer; opacity: 1!important;}
-  .back>span{position: absolute; top: 50%; left: 50%; z-index: -1; transform: translate(-50%, -50%); line-height: 30px; transition: all .3s linear}
-  .back:hover >span{color: #72b182}
+html {
+  min-height: 100vh;
+}
+* {
+  box-sizing: border-box;
+}
+.css_all {
+  height: 92vh;
+}
+#impress-toolbar {
+  opacity: 1 !important;
+}
+#shape {
+  stroke-width: 6px;
+  fill: transparent;
+  stroke: #222;
+  stroke-dasharray: 104 300;
+  stroke-dashoffset: -220;
+  transition: 1s all ease;
+}
+svg:hover #shape {
+  stroke-dasharray: 70 0;
+  stroke-width: 3px;
+  stroke-dashoffset: 0;
+  stroke: #72b182;
+}
+.back {
+  position: fixed;
+  bottom: 2px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 150px;
+  height: 52px;
+  font-size: 14px;
+  cursor: pointer;
+  opacity: 1 !important;
+}
+.back > span {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: -1;
+  transform: translate(-50%, -50%);
+  line-height: 30px;
+  transition: all 0.3s linear;
+}
+.back:hover > span {
+  color: #72b182;
+}
 </style>

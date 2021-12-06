81e1cf6fb4e2993e41628105b892a889cf5f95e7
kora <kangrun@hhx-info.com>
2021-12-06 09:30:18 +0800

    resume - 路由打包配置问题

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,20 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  // hash地址 去除#
-  mode: 'history',
-  routes: [
-    {
-      name: 'home',
-      path: '/',
-      meta: {title: '', keepAlive: false},
-      component: (resolve) => require(['../pages/home.vue'], resolve)
-    },
-    {
-      name: 'php',
-      path: '/php',
-      meta: {title: '', keepAlive: false},
-      component: (resolve) => require(['../pages/php.vue'], resolve)
-    }
-  ]
+	// hash地址 去除#
+	// mode: 'history',
+	routes: [
+		{
+			name: 'home',
+			path: '/',
+			meta: { title: '', keepAlive: false },
+			component: (resolve) => require(['../pages/home.vue'], resolve)
+		},
+		{
+			name: 'php',
+			path: '/php',
+			meta: { title: '', keepAlive: false },
+			component: (resolve) => require(['../pages/php.vue'], resolve)
+		}
+	]
 })
